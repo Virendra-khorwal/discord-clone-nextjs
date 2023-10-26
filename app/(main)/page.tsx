5ca5179293f22d3f3a034ea304901c340a594def
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-10-26 12:26:34 +0530

    prisma and mongodb configured

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from '@clerk/nextjs'
+import { ModeToggle } from '@components/mode-toggle'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl='/' />
+      <ModeToggle />
     </div>
   )
 }
